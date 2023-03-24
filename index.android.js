e486a977cc2114ac08787f16173f891881f43806
mahesh.vishwakarma@rsystems.com <mahesh.vishwakarma@rsystems.com>
2023-03-24 10:48:00 +0530

    seek issue fixed

--- index.android.js
+++ index.android.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
-import { Platform} from 'react-native';
 import AppAndroidPlayer from './Video.App';
-console.log("Platform.OS", Platform.OS)
-// const Module = Platform.OS === 'web' ? require('./Video.web') : require('./Video.App')
-
-export default class AndroidPlayer extends Component {
+class AndroidPlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return <AppAndroidPlayer {...this.props} />
+    return <AppAndroidPlayer {...this.props}  ref={this.props.innerRef} />
     
   }
 }
+
+export default React.forwardRef((props, ref) => <AndroidPlayer 
+  innerRef={ref} {...props}
+/>);
--- index.ios.js
+++ index.ios.js
@@ -4,14 +4,18 @@ import AppPlayer from './Video.App';
 console.log("Platform.OS", Platform.OS)
 // const Module = Platform.OS === 'web' ? require('./Video.web') : require('./Video.App')
 
-export default class Player extends Component {
+class Player extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-      return <AppPlayer  {...this.props}/>
+      return <AppPlayer  {...this.props} ref={this.props.innerRef}/>
     
   }
 }
+
+export default React.forwardRef((props, ref) => <Player 
+  innerRef={ref} {...props}
+/>);
