5d273ca1dff645075a0180e0ccfa4cb5a6f3d002
leena <leena@Leena-rsis-MacBook-Pro.local>
2023-12-06 14:28:02 +0530

    added missing bracket

--- assets/Icon/icon.js
+++ assets/Icon/icon.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Platform } from "react-native";
 import { RFPercentage } from "react-native-responsive-fontsize";
 
 export const ImageIcon = ({ name, size, style, color, solid }) => {
 
   return <Icon name={name} size={size} style={style} color={color} solid={solid ? true : false} />;
-
-
-export function normalize(size) {
-  if (Platform.isTV) {
-    return RFPercentage(size);
-
-  } else {
-
-    return RFPercentage(size);
-
-  }
-
-
-
 }
 
-
-
-
+export function normalize(size) {
+  RFPercentage(size);
+}
 
 export function checkArrayAndElements(array) {
 
